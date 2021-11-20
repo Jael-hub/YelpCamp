18e8e2b8c1f775611a564d12868b4b5972019be5
Jael <jaelbisogni@gmail.com>
2021-11-20 14:11:14 -0300

    Enter

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,11 +4,16 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
 const campgrounds = require('../controllers/campgrounds')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(upload.array('image'),(req, res)=>{
+        console.log(req.body, req.files);
+        res.send('it worked')
+    })
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
