6f086cffde2f044bf1125e051a0e89f57ad67879
Jael <deadlyomens@gmail.com>
2021-12-06 20:03:51 -0300

    fixed security issues

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -45,6 +47,7 @@ app.use(express.static(path.join(__dirname,'public') ));
 
 const sessionConfig={
     secret:'thisshouldbeabettersecret',
+    name:'blahhhhh',
     resave:false,
     saveUnitialized:true,
     cookie:{
@@ -54,9 +57,57 @@ const sessionConfig={
 }
 app.use(session(sessionConfig))
 app.use(flash());
+
 app.use(express.urlencoded({extended:true}));
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+//This is the array that needs added to
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dt60rgaak/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(mongoSanitize());
+
 
 ///////
 passport.use(new LocalStrategy(User.authenticate()));
@@ -66,7 +117,7 @@ passport.deserializeUser(User.deserializeUser());
 ////////////////////////////////////////////
 
 app.use((req, res, next) => {
-    
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
